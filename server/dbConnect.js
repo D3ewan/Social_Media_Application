ca7d4cc8be13875cf3eff398ba7c8d81c5a593f1
Deewan Singh <deewansingh827@gmail.com>
2024-01-22 11:16:54 +0530

    fix bug

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = async () => {
-    const url = `mongodb+srv://${process.env.MONGOOSE_ID}:${process.env.MONGOOSE_PASSWORD}@cluster0.3r80h15.mongodb.net/socialMedia?retryWrites=true&w=majority`;
+    const url = `mongodb+srv://${process.env.MONGOOSE_ID}:${process.env.MONGOOSE_PASSWORD}@cluster0.ygxeoeu.mongodb.net/SocialMedia?retryWrites=true&w=majority`;
     try {
         await mongoose.connect(url)
         console.log(`mongodb connected ${mongoose.connection.host} : ${mongoose.connection.port}`)
